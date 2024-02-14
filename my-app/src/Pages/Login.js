d793c40deb9a0740345e625f7eb3c119dbd5c187
eileent7129 <eat7129@nyu.edu>
2024-02-13 22:22:27 -0500

    organized files and folders

--- my-app/src/Pages/Login.js
+++ my-app/src/Pages/Login.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { BACKEND_URL } from "./constants";
+import { BACKEND_URL } from "../constants";
 const USERS_ENDPOINT = `${BACKEND_URL}/users`;
-import "./App.css";
+// import "./App.css";
 
 
 function usersObjectToArray({ Data }) {
@@ -13,28 +13,36 @@ function usersObjectToArray({ Data }) {
   return users;
 }
 
+function ErrorMessage({ message }) {
+	return (
+	  <div className="error-message">
+		{message}
+	  </div>
+	);
+  }
+
 function Login() {
   const [users, setUsers] = useState([]);
+  const [error, setError] = useState('');
 
   const fetchUsers = () => {
     axios
       .get(USERS_ENDPOINT)
       .then(({ data }) => setUsers(usersObjectToArray(data)))
       .catch(() =>
-        setError("There was a problem retrieving the list of games.")
+        setError("There was a problem retrieving the users.")
       );
   };
 
   useEffect(fetchUsers, []);
   console.log("These are the users: ", users);
+  console.log()
 
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="Users" element={<Users />} />
-      </Routes>
-    </BrowserRouter>
+	<>
+	<h2>Login</h2>
+	{error && <ErrorMessage message={error} />}
+	</>
   );
 }
 

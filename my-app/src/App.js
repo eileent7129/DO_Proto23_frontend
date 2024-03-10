56c23be94d6fe23ce440d5d8f1114fb34652f330
eileent7129 <eat7129@nyu.edu>
2024-03-10 19:37:28 -0400

    styled sign up page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
+import Home from "./Pages/Home";
 import Dashbaord from "./Pages/Dashboard";
 
 function App() {
   return (
     <>
       {/* <Login /> */}
-	  {/* <SignUp /> */}
+	  <SignUp />
+	  {/* <Home /> */}
 	  {/* <Dashbaord /> */}
     </>
   );

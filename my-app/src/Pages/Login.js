cf6714a53e7a6a4eae889b679dfef997cc57dbd5
Cat Almuete <ca2318@nyu.edu>
2024-03-31 23:10:06 -0400

    Added nav bar to Login.js

--- my-app/src/Pages/Login.js
+++ my-app/src/Pages/Login.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { BACKEND_URL } from "../constants";
+import Home from "./Home";
+import SignUp from "./SignUp";
 const USERS_ENDPOINT = `${BACKEND_URL}/users`;
 // import "./App.css";
 
@@ -21,7 +23,7 @@ function ErrorMessage({ message }) {
 		{message}
 	  </div>
 	);
-  }
+}
 
 function Login() {
   const [users, setUsers] = useState([]);
@@ -42,10 +44,28 @@ function Login() {
   console.log()
 
   return (
-	<>
-	<h2>Login</h2>
-	{error && <ErrorMessage message={error} />}
-	</>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/signup">Sign Up</a>
+          </li>
+        </ul>
+      </nav>
+
+      <h2>Login</h2>
+      {error && <ErrorMessage message={error} />}
+
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 

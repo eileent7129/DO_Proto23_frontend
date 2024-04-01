c5490598a37a594f6937c84b84e625c993af49fb
Sunny Jeong <sj3048@nyu.edu>
2024-03-31 20:25:41 -0400

    Navigation to Followers

---
+++ my-app/src/Pages/Followers.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { BACKEND_URL } from "../constants";
+const USERS_ENDPOINT = `${BACKEND_URL}/users`;
+
+const user_info = {
+	name: "Jane Doe",
+	username: "janedoe",
+	res_hall: "Carlyle Court",
+	address: "25 Union Square W, New York, NY",
+	followers_number: 350,
+	following_number: 55
+}
+
+
+export default function Followers() {
+	return (
+		<>
+        
+        <h2>Followers</h2>
+
+
+		<div className="user-container">
+			<div className="user-photo">
+
+			</div>
+			<div className="user-info"> 
+			<h2>{user_info.name}</h2>
+			<p>@{user_info.username}</p>
+			<p>{user_info.res_hall}, {user_info.address}</p>
+			<div className="following">
+				<p>{user_info.followers_number} followers</p>
+				<p>{user_info.following_number} following</p>
+			</div>
+			</div>
+		</div>
+	</>
+
+	)
+	
+}

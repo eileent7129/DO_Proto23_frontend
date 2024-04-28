38c11102d5d3a08b3585fe37ad6d592b1b0cb4d9
Eileen Twimasi <eat7129@nyu.edu>
2024-04-28 16:39:14 -0400

    click on user id to navigate to their profile

--- my-app/src/Pages/ViewItem.js
+++ my-app/src/Pages/ViewItem.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Button from "@mui/material/Button";
+import {useNavigate} from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { BACKEND_URL } from "../constants";
 const PRODUCTS_ENDPOINT = `${BACKEND_URL}/product`;
 
 export default function ViewItem() {
+	const navigate = useNavigate();
 	const location = useLocation();
 	const prodInfo = location.state.product;
 
@@ -23,7 +26,7 @@ export default function ViewItem() {
 					<p>Catagories: {prodInfo.categories}</p>
 					<p>Condition: {prodInfo.condition}</p>
 					<p>Date Posted: {prodInfo.date_posted}</p>
-					<p>{prodInfo.userID} "{prodInfo.comments}"</p>
+					<p><Button onClick={() => navigate(`/user/${prodInfo.user_id}`)}>{prodInfo.user_id}</Button> "{prodInfo.comments}"</p>
 				</div>
 			</div>
 		</>
